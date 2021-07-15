9b06bf1b52898f318b2ad41c3f8121c7214f22eb
Saulo <sauloveigr@hotmail.com>
2021-07-15 18:34:31 -0300

    Create lesson 2.6

---
+++ 2 - Programação Orientada a Objeto/Aula 06 - Métodos de Acesso/script.js
@@ -0,0 +1,35 @@
+/* Aula 06 - Métodos de Acesso */
+
+class Quadrado {
+    constructor(base, altura) {
+        let cor = 'blue'
+        this.base = base
+        this.altura = altura
+        this.getCor = () => {return cor}
+        this.setCor = (c) => {cor = c} // para alterar valor de cor
+    }
+}
+
+const quadrado = new Quadrado(3, 4)
+
+quadrado.setCor('red') // altera para red
+
+console.log(quadrado.getCor())
+
+/* Método de acesso */
+
+class Quadrado2 {
+
+    constructor(base, altura) {
+        this._cor = 'blue'
+        this.base = base
+        this.altura = altura
+    }
+
+    get cor() {return this._cor}
+    set cor(cor) {this._cor = cor}
+}
+
+const quadrado2 = new Quadrado2 
+quadrado2.cor = 'red' // set cor
+console.log(quadrado2.cor)
