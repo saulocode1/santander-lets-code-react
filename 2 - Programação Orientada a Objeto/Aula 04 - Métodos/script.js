a46f73b96783c02fd93c2031f217fd2f87321d26
Saulo <sauloveigr@hotmail.com>
2021-07-15 16:29:05 -0300

    Create lesson 2.4

--- 2 - Programação Orientada a Objeto/Aula 04 - Métodos/script.js
+++ 2 - Programação Orientada a Objeto/Aula 04 - Métodos/script.js
@@ -1,18 +1,27 @@
+/* Métodos
+
+    Chamamos de métodos e não funções por dois motivos:
+    1 - Por estar no contexto POO e no interior de uma classe.
+    2 - Por ele poder manipular o estado interno de um objeto.
+
+*/
+
 class Retângulo {
     constructor(base, altura) {
         this.base = base;
         this.altura = altura;
         this.cor = undefined;
     }
-    calculaArea() {
+    calculaArea() { // método
         return this.base * this.altura
     }
-    duplicaBase() {
+    duplicaBase() { // método
         this.base = this.base * 2
     }
 }
 
 const retângulo = new Retângulo(10, 5)
-retângulo.duplicaBase()
-console.log(retângulo.calculaArea())
 
+retângulo.duplicaBase()
+
+console.log(retângulo.calculaArea())
